a7094d35b48e73d5da6fdbf6e853c8b7f93b0c44
qiyi-prog <2970903525@qq.com>
2026-05-28 12:43:13 +0800

    添加缺失的HarmonyOS项目配置文件

---
+++ entry/hvigorfile.ts
@@ -0,0 +1,6 @@
+import { build } from '@ohos/hvigor';
+import { UntarArkUI } from '@ohos/hvigor-ohos-plugin';
+
+export default build({
+  plugins: [new UntarArkUI()]
+})
---
+++ hvigorfile.ts
@@ -0,0 +1,3 @@
+import { build } from '@ohos/hvigor';
+
+export default build({})
